6250ef62b30feee5afaf65ee0cdb25c33a77fcb4
Jesutooni Oguntula <joguntula@gmail.com>
2025-04-18 17:18:39 -0500

    Implemented playRound function

--- play.js
+++ play.js
@@ -4,10 +4,13 @@ function getComputerChoice() {
     randInt = Math.floor(Math.random() * 3) + 1;
     if (randInt == 1) {
         console.log("Rock!")
+        return "Rock!"
     } else if (randInt == 2) {
         console.log("Paper!")
+        return "Paper!"
     } else {
         console.log("Scissors!")
+        return "Scissors!"
     }
 }
 getComputerChoice();
@@ -15,12 +18,26 @@ getComputerChoice();
 function getHumanChoice() {
     choice = prompt("Make your choice: ");
     randChoice = choice.toLowerCase();
-    if (randChoice == "r") {
+    if (randChoice == "rock") {
         console.log("Rock!")
-    } else if (randChoice == "p") {
+        return "Rock!"
+    } else if (randChoice == "paper") {
         console.log("Paper!")
-    } else if (randChoice == "s") {
+        return "Paper!"
+    } else if (randChoice == "scissors") {
         console.log("Scissors!")
+        return "Scissors!"
     }
 }
 getHumanChoice();
+
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice, computerChoice) {
+    console.log(`You lose! ${computerChoice} beats ${humanChoice}`);
+}
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);

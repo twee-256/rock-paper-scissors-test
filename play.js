9e270b94fd384f1eb2f2eb65f090a7efc595c3dc
Jesutooni Oguntula <joguntula@gmail.com>
2025-05-04 01:38:57 -0500

    Added UI and reworked previous code.

--- play.js
+++ play.js
@@ -1,15 +1,11 @@
-console.log("Hello World!")
 
 function getComputerChoice() {
     randInt = Math.floor(Math.random() * 3) + 1;
     if (randInt == 1) {
-        console.log("Rock")
         return "Rock"
     } else if (randInt == 2) {
-        console.log("Paper")
         return "Paper"
     } else {
-        console.log("Scissors")
         return "Scissors"
     }
 }
@@ -17,82 +13,141 @@ function getComputerChoice() {
 function getHumanChoice() {
     let randChoice = prompt("Make your choice: ").toLowerCase();
     if (randChoice == "rock") {
-        console.log("Rock")
         return "Rock"
     } else if (randChoice == "paper") {
-        console.log("Paper")
         return "Paper"
     } else if (randChoice == "scissors") {
-        console.log("Scissors")
         return "Scissors"
     }
 }
 
 let humanScore = 0;
 let computerScore = 0;
+let gameCount = 0;
+
+let scoreCard = document.querySelector('#score');
+let outputCard = document.querySelector('#outputMsg');
+let selection = document.querySelector('#selection');
+let computerSelection = getComputerChoice();
+
+let restartCard = document.querySelector('#restart');
+restartCard.style.display = "none";
+
+let round = document.createElement("p");
+round.classList.add("round");
+round.textContent = "Round: " + gameCount;
+
+let hScore = document.createElement("p");
+hScore.classList.add("hScore");
+hScore.textContent = "Your score: " + humanScore;
+
+let cScore = document.createElement("p");
+cScore.classList.add("cScore");
+cScore.textContent = "Opponent's score: " + computerScore;
+
+let output = document.createElement("p");
+output.classList.add("output");
+
+scoreCard.appendChild(round);
+scoreCard.appendChild(hScore);
+scoreCard.appendChild(cScore);
+outputCard.appendChild(output);
+
+selection.addEventListener('click', (event) => {
+    let choice = event.target;
+    let choiceId = choice.id;
+
+    switch (choiceId) {
+        case 'rock':
+            playRound(choiceId, computerSelection);
+            break;
+        case 'scissors':
+            playRound(choiceId, computerSelection);
+            break;
+        case 'paper':
+            playRound(choiceId, computerSelection);
+            break;
+    }
+})
+
+let play = document.querySelector('#play');
+play.addEventListener('click', () => {
+    restart();
+});
+
+// Functions...
 
 function playRound(humanChoice, computerChoice) {
-
     let regRock = /rock/i;
     if (humanChoice.match(regRock)) {
         if (computerChoice == "Rock") {
-            console.log("Tie!")
+            output.textContent = "It's a Tie!";
+            updateScore();
         } else if (computerChoice == "Paper") {
-            console.log("You lose! Paper beats Rock.")
+            output.textContent = "You lose! Paper beats Rock.";
             computerScore += 1;
+            updateScore();
         } else {
-            console.log("You win! Rock beats Scissors.")
+            output.textContent = "You win! Rock beats Scissors.";
             humanScore += 1;
+            updateScore();
         }
     }
 
     let regPaper = /paper/i;
     if (humanChoice.match(regPaper)) {
         if (computerChoice == "Paper") {
-            console.log("Tie!")
+            output.textContent = "It's a Tie!";
+            updateScore();
         } else if (computerChoice == "Rock") {
-            console.log("You win! Paper covers Rock.")
             humanScore += 1;
+            output.textContent = "You win! Paper covers Rock.";
+            updateScore();
         } else {
-            console.log("You lose! Scissors cuts Paper.")
             computerScore += 1;
+            output.textContent = "You lose! Scissors cuts Paper.";
+            updateScore();
         }
     }
 
     let regScissors = /scissors/i;
     if (humanChoice.match(regScissors)) {
         if (computerChoice == "Scissors") {
-            console.log("Tie!")
-        } else if (computerChoice == "Saper") {
-            console.log("You win! Scissors cut Paper.")
+            output.textContent = "It's a Tie!";
+            updateScore();
+        } else if (computerChoice == "Paper") {
             humanScore += 1;
+            output.textContent = "You win! Scissors cut Paper.";
+            updateScore();
         } else {
-            console.log("You lose! Rock beats Scissors.")
             computerScore += 1;
+            output.textContent = "You lose! Rock beats Scissors.";
+            updateScore();
         }
     }
-    console.log(humanScore);
-    console.log(computerScore);
+
+    gameCount += 1;
+    round.textContent = "Round: " + gameCount;
+
+    outputMsg();
 }
 
-function playGame() {
-    let gameCount = 0;
-
-    for (i = 0; i < 5; i++) {
-        let humanSelection = getHumanChoice();
-        let computerSelection = getComputerChoice();
-
-        gameCount += 1;
-        playRound(humanSelection, computerSelection);
-    }
-
-    if (humanScore > computerScore) {
-        console.log(`You win with ${humanScore} points!`);
-    } else if (humanScore == computerScore) {
-        console.log(`It's a tie! You have ${humanScore} points; your opponent has ${computerScore} points.`)
-    } else {
-        console.log(`You lose with your opponent having ${computerScore} wins :( Better luck next time!`)
+function outputMsg() {
+    if (humanScore == 5 || computerScore == 5) {
+        selection.style.display = "none";
+        scoreCard.style.display = "none";
+        let message = humanScore == 5 ? "You Win with 5 points!" : "Your opponent wins with 5 points, better luck next time!";
+        output.textContent = message;
+        restartCard.style.display = "block";
     }
 }
 
-playGame();
+function updateScore() {
+    hScore.textContent = "Your score: " + humanScore;
+    cScore.textContent = "Opponent's score: " + computerScore;
+}
+
+function restart() {
+    location.reload();
+    restartCard.style.display = "none";
+}
